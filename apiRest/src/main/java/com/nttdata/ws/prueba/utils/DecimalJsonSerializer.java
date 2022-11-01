ccf183f2a7500b6b33db8308d440688cacfdbe24
MaciasSanchez <72670031+MaciasSanchez@users.noreply.github.com>
2022-11-01 10:30:55 -0500

    ajustes

--- apiRest/src/main/java/com/nttdata/ws/prueba/utils/DecimalJsonSerializer.java
+++ apiRest/src/main/java/com/nttdata/ws/prueba/utils/DecimalJsonSerializer.java
@@ -18,6 +18,6 @@ public class DecimalJsonSerializer extends JsonSerializer<Double> {
 	  @Override
 	  public void serialize(Double value, JsonGenerator jgen, SerializerProvider provider) 
 	    throws IOException, JsonProcessingException {
-	      jgen.writeString( String.format("%.2f", value));
+	      jgen.writeString(String.format("%.2f", value));
 	  }
 }
--- apiRest/src/test/java/com/nttdata/ws/prueba/utils/DecimalJsonSerializerTest.java
+++ apiRest/src/test/java/com/nttdata/ws/prueba/utils/DecimalJsonSerializerTest.java
@@ -6,19 +6,29 @@ package com.nttdata.ws.prueba.utils;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nttdata.ws.prueba.model.EstadoCuentaType;
 
 /**
  * @author Angelica
  *
  */
+@SpringBootTest
 class DecimalJsonSerializerTest {
 
 	/**
 	 * Test method for {@link com.nttdata.ws.prueba.utils.DecimalJsonSerializer#serialize(java.lang.Double, com.fasterxml.jackson.core.JsonGenerator, com.fasterxml.jackson.databind.SerializerProvider)}.
 	 */
 	@Test
-	void testSerializeDoubleJsonGeneratorSerializerProvider() {
-		fail("Not yet implemented");
+	void testSerializeDoubleJsonGeneratorSerializerProvider() throws JsonProcessingException {
+		EstadoCuentaType estadoCta = new EstadoCuentaType();
+		estadoCta.setSaldoDisponibleCorte(Double.valueOf("20.3"));
+
+	     ObjectMapper mapper = new ObjectMapper();
+	     assertEquals("{\"reporte\":\"ESTADO DE CUENTA\",\"nombreCliente\":null,\"fechaInicioCorte\":null,\"fechaFinCorte\":null,\"detalleMovimientos\":null,\"montoTotalDepositos\":\"0,00\",\"montoTotalRetiros\":\"0,00\",\"saldoDisponibleCorte\":\"20,30\"}", mapper.writeValueAsString(estadoCta));
 	}
 
 }
